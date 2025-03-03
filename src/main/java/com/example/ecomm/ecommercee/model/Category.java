2a19a061012e3e6123429e0840918de87bd115c7
Swatiiiishettyyyy <Shettyswati711@gmail.com>
2025-03-03 20:36:02 +0530

    used lombok to reduce boiler code

--- src/main/java/com/example/ecomm/ecommercee/model/Category.java
+++ src/main/java/com/example/ecomm/ecommercee/model/Category.java
@@ -1,35 +1,19 @@
 package com.example.ecomm.ecommercee.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity(name = "categories")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 
-    @Version
-    private Long version;
 
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
 }
