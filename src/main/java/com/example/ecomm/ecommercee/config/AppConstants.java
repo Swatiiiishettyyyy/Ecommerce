b947aa7089b249042e26c04ea6965a5ad90314bc
Swatiiiishettyyyy <Shettyswati711@gmail.com>
2025-03-05 18:16:00 +0530

    Added default constants for pagination

---
+++ src/main/java/com/example/ecomm/ecommercee/config/AppConstants.java
@@ -0,0 +1,6 @@
+package com.example.ecomm.ecommercee.config;
+
+public class AppConstants {
+    public static final String PAGE_NUMBER ="0";
+    public static final String PAGE_SIZE ="50";
+}
--- src/main/java/com/example/ecomm/ecommercee/controller/CategoryController.java
+++ src/main/java/com/example/ecomm/ecommercee/controller/CategoryController.java
@@ -1,5 +1,7 @@
 package com.example.ecomm.ecommercee.controller;
 
+import com.example.ecomm.ecommercee.config.AppConfig;
+import com.example.ecomm.ecommercee.config.AppConstants;
 import com.example.ecomm.ecommercee.model.Category;
 import com.example.ecomm.ecommercee.payload.CategoryDTO;
 import com.example.ecomm.ecommercee.payload.CategoryResponse;
@@ -36,8 +38,8 @@ public class CategoryController {
     @GetMapping("/public/categories")
     //@RequestMapping(value = "/public/categories", method = RequestMethod.GET)
     public ResponseEntity<CategoryResponse> getAllCategories(
-            @RequestParam(name="pageNumber") Integer pageNumber,
-            @RequestParam(name="pageSize") Integer pageSize) {
+            @RequestParam(name="pageNumber",defaultValue = AppConstants.PAGE_NUMBER,required = false) Integer pageNumber,
+            @RequestParam(name="pageSize", defaultValue = AppConstants.PAGE_SIZE,required = false) Integer pageSize) {
         CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber,pageSize);
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
