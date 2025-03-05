7ee128b4dfd5da26fb73d631a453cf9bfacc71fc
Swatiiiishettyyyy <Shettyswati711@gmail.com>
2025-03-05 14:38:35 +0530

    changed to handle using DTo used category dto

--- src/main/java/com/example/ecomm/ecommercee/service/CategoryServiceImpl.java
+++ src/main/java/com/example/ecomm/ecommercee/service/CategoryServiceImpl.java
@@ -3,8 +3,11 @@ package com.example.ecomm.ecommercee.service;
 import com.example.ecomm.ecommercee.exceptions.APIException;
 import com.example.ecomm.ecommercee.exceptions.ResourceNotFoundException;
 import com.example.ecomm.ecommercee.model.Category;
+import com.example.ecomm.ecommercee.payload.CategoryDTO;
+import com.example.ecomm.ecommercee.payload.CategoryResponse;
 import com.example.ecomm.ecommercee.repositories.CategoryRepository;
 import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
@@ -23,10 +26,18 @@ public class CategoryServiceImpl implements CategoryService{
     @Autowired
     private CategoryRepository categoryRepository;
 
-    @Override
-    public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
-    }
+    @Autowired
+    private ModelMapper modelMapper;
+
+//    @Override
+//    public List<Category> getAllCategories() {
+//        List<Category> categories = categoryRepository.findAll();
+//        if(categories.isEmpty())
+//            throw new APIException("No category created till now");
+//        return categories;
+//    }
+
+
 
     @Override
     public void createCategory(Category category) {
@@ -53,19 +64,37 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
-        List<Category> categories = categoryRepository.findAll();
+//        List<Category> categories = categoryRepository.findAll();
+//
+//        Optional<Category> optionalCategory = categories.stream()
+//                .filter(c -> c.getCategoryId().equals(categoryId))
+//                .findFirst();
+//
+//        if(optionalCategory.isPresent()){
+//            Category existingCategory = optionalCategory.get();
+//            existingCategory.setCategoryName(category.getCategoryName());
+//            Category savedCategory = categoryRepository.save(existingCategory);
+//            return savedCategory;
+//        } else {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
+//        }
+        Category savedCategory = categoryRepository.findById(categoryId)
+                .orElseThrow(()->new ResourceNotFoundException("category", "categoryId", categoryId));
+        category.setCategoryId(categoryId);
+        savedCategory=categoryRepository.save(category);
+        return savedCategory;
+    }
 
-        Optional<Category> optionalCategory = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst();
-
-        if(optionalCategory.isPresent()){
-            Category existingCategory = optionalCategory.get();
-            existingCategory.setCategoryName(category.getCategoryName());
-            Category savedCategory = categoryRepository.save(existingCategory);
-            return savedCategory;
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
-        }
+    @Override
+    public CategoryResponse getAllCategories() {
+        List<Category> categories= categoryRepository.findAll();
+        if(categories.isEmpty())
+            throw new APIException("No category created till now");
+        List<CategoryDTO> categoryDTOS= categories.stream()
+                .map(category -> modelMapper.map(category, CategoryDTO.class))
+                .toList();
+        CategoryResponse categoryResponse = new CategoryResponse();
+        categoryResponse.setContent(categoryDTOS);
+        return categoryResponse;
     }
 }
