899ac93450b97dc785080ab96a02ae77666fb89d
Swatiiiishettyyyy <Shettyswati711@gmail.com>
2025-03-05 18:38:02 +0530

    Added Sorting

--- src/main/java/com/example/ecomm/ecommercee/service/CategoryServiceImpl.java
+++ src/main/java/com/example/ecomm/ecommercee/service/CategoryServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
@@ -73,8 +74,12 @@ public class CategoryServiceImpl implements CategoryService{
 
 
     @Override
-    public CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize) {
-        Pageable pageDetails = PageRequest.of(pageNumber,pageSize);
+    public CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder) {
+        Sort sortByAndOrder = sortOrder.equalsIgnoreCase("asc")
+                ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
+
+        Pageable pageDetails = PageRequest.of(pageNumber,pageSize,sortByAndOrder);
         Page<Category> categoryPage= categoryRepository.findAll(pageDetails);
         List<Category> categories= categoryPage.getContent();
         //List<Category> categories= categoryRepository.findAll();
